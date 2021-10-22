f00cd4738c97f22603f32a0f911f63d691d94fb3
maxhalde98 <max.halde@gmail.com>
2021-10-22 16:51:07 -0400

    Added computerPlay function, initial files

---
+++ script.js
@@ -0,0 +1,10 @@
+const ROCK = 'Rock';
+const PAPER = 'Paper';
+const SCISSORS = 'Scissors';
+
+const CHOICES = [ROCK,PAPER,SCISSORS];
+
+function computerPlay() {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
